b50244f53e116737759384624dd839b4d7be24b7
kthuy1312 <lythuy13122004@gmail.com>
2026-05-08 21:15:37 +0700

    modal booking và voucher

---
+++ be/src/models/Booking.js
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    concertId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Concert",
+      required: true,
+    },
+
+    ticketTypeId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "TicketType",
+      required: true,
+    },
+
+    voucherId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Voucher",
+      default: null,
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    unitPrice: {
+      type: Number,
+      required: true,
+    },
+
+    discountAmount: {
+      type: Number,
+      default: 0,
+    },
+
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "RECEIVED",
+        "RESERVED",
+        "WAITING_PAYMENT",
+        "CONFIRMED",
+        "CANCELLED",
+        "EXPIRED",
+      ],
+      default: "RECEIVED",
+    },
+
+    //mã định danh duy nhất
+    //để đảm bảo 1 request chỉ được xly 1 lần, dù user có gửi lại nhiều lần
+    idempotencyKey: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+
+    reservedAt: Date,
+    expiredAt: Date,
+    completedAt: Date,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+bookingSchema.index({ userId: 1 });
+bookingSchema.index({ concertId: 1 });
+bookingSchema.index({ ticketTypeId: 1 });
+bookingSchema.index({ status: 1 });
+
+//kh cho duplicate booking cực quan trọng
+bookingSchema.index({ idempotencyKey: 1 }, { unique: true });
+
+const Booking = mongoose.model("Booking", bookingSchema);
+export default Booking;
---
+++ be/src/models/Voucher.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+
+const voucherSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      uppercase: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    //kiểu giảm giá (theo % hay cố định tiền    )
+    discountType: {
+      type: String,
+      enum: ["PERCENT", "FIXED"],
+      required: true,
+    },
+
+    //giá trị giảm cụ thể
+    //nếu là PERCENT: discountValue = 10 giảm 10%
+    //nếu là FIXED: discountValue = 50000 → giảm 50,000đ
+    discountValue: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    //giới hạn số lần voucher được sử dụng
+    maxUsage: {
+      type: Number,
+      required: true,
+    },
+
+    currentUsage: {
+      type: Number,
+      default: 0,
+    },
+
+    minOrderAmount: {
+      type: Number,
+      default: 0,
+    },
+
+    validFrom: {
+      type: Date,
+      required: true,
+    },
+
+    validUntil: {
+      type: Date,
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+voucherSchema.index({ code: 1 }, { unique: true });
+voucherSchema.index({ isActive: 1 });
+voucherSchema.index({ validFrom: 1, validUntil: 1 });
+
+const Voucher = mongoose.model("Voucher", voucherSchema);
+export default Voucher;
